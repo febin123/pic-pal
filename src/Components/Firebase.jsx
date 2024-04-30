2f9e2b9a20b9d6eca9970e6bf9e8803dbf8e3c1d
febin123 <febsep96@gmail.com>
2024-04-30 16:43:59 +0530

    connected firebase to the project

---
+++ src/Components/Firebase.jsx
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyAXuuA6825gUQ8yQk8bnK3gkt29vzCDLJw",
+        authDomain: "pic-pal.firebaseapp.com",
+        projectId: "pic-pal",
+        storageBucket: "pic-pal.appspot.com",
+        messagingSenderId: "631214086040",
+        appId: "1:631214086040:web:45f7c9bf61e1b356057ded",
+        measurementId: "G-J5BTPVZDXE"
+      };
+  
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
